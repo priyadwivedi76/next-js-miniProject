7c4c5f79cbaa936a13748268b897c37b3c012288
Dwivedi Priya <pd7668665@gmail.com>
2026-01-02 19:41:25 +0530

    quote-of-day

---
+++ quote-of-the-day/app/page.js
@@ -0,0 +1,9 @@
+import QuoteCard from "@/components/QuoteCard";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-white text-black">
+      <QuoteCard/>
+    </div>
+  );
+}
---
+++ quote-of-the-day/components/QuoteCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const QuoteCard = async() => {
+
+const API_KEY='OnrtNJiKO3aEXihBVUqDgA==WHDTm7UQfu5VZNkF'
+const res=await fetch(`https://api.api-ninjas.com/v2/quoteoftheday?X-Api-Key=${API_KEY}`)
+const [data]=await res.json();
+
+  return (
+    <div className='bg-yellow-100 w-96 h-48 rounded-lg border-2 border-red-200 shadow-lg'>
+        <h1 className='text-red-800 text-2xl font-semibold text-center mt-4'>{data.quote}</h1>
+        <h3 className='text-red-800 text-md font-medium text-right m-3 hover:underline transition ease-in-ease-out'>-{data.author}</h3>
+        <div className='flex gap-1 text-sm mx-3 mt-10 opacity-[70%] hover:opacity-[100%] transition ease-in-ease-out'>
+            <h1 className='text-red-500 text-bold'>Category:</h1>
+            {data.categories.map((items)=>(
+                <p key={items}>{items},</p>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default QuoteCard
